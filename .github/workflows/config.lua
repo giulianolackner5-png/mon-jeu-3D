3168abe4168ed1c2cbce89cc7983a93e3ff388b3
giulianolackner5-png <giulianolackner5@gmail.com>
2026-06-01 16:54:51 +0200

    Create config.lua

---
+++ .github/workflows/config.lua
@@ -0,0 +1,11 @@
+Config = {
+    player = {
+        speed = 200,      -- Vitesse du carré
+        size = 30,        -- Taille du carré
+        color = {1, 0, 0} -- Couleur Rouge (Format RGB : Rouge, Vert, Bleu entre 0 et 1)
+    },
+    game = {
+        gravity = 500,    -- Force de la gravité
+        title = "Mon Jeu CIA 3DS"
+    }
+}
